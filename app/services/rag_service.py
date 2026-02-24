0db70786f92837e59f960de66d52e2c3bb5c6eba
venkatesh reddy <venkatesh.reddy@easystepin.com>
2026-02-24 18:48:39 +0530

    WIP: saving my current changes

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,24 +1,41 @@
-import openai
-from app.core.config import settings
-from app.services.vector_store import query_documents
-openai.api_key = settings.OPENAI_API_KEY
-# client = OpenAI(api_key=settings.OPENAI_API_KEY)
+# import openai
+# from app.core.config import settings
+# from app.services.vector_store import query_documents
+# openai.api_key = settings.OPENAI_API_KEY
+# # client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
-def generate_answer(question: str):
-    docs = query_documents(question)
-    context = "\n".join(docs)
+# def generate_answer(question: str):
+#     docs = query_documents(question)
+#     context = "\n".join(docs)
 
-    prompt = f"""
-    Answer only from the context below:
+#     prompt = f"""
+#     Answer only from the context below:
 
-    {context}
+#     {context}
 
-    Question: {question}
-    """
+#     Question: {question}
+#     """
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}]
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[{"role": "user", "content": prompt}]
+#     )
+
+#     return response["choices"][0]["message"]["content"]
+
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def generate_answer(question: str) -> str:
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": question}
+        ],
+        temperature=0.7,
     )
 
-    return response["choices"][0]["message"]["content"]
+    return response.choices[0].message.content
